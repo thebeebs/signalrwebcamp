dea711342ede9cc6f10a6e01489bba00d1cb1b9f
thebeebs <martinbeeby@hotmail.com>
2014-12-11 13:10:19 +0000

    Push Files Live

---
+++ WebApplication24/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication24
+{
+    public class ChatHub : Hub
+    {
+        static List<Tuple<string, string>> messages;
+
+        public ChatHub()
+        {
+
+            if (messages == null)
+            {
+                messages = new List<Tuple<string, string>>();
+            }
+        }
+        public void Logon()
+        {
+            Clients.Caller.loadHistory(messages);
+        }
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            messages.Add(new Tuple<string, string>(name, message));
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ WebApplication24/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication24.StartUp))]
+
+namespace WebApplication24
+{
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
